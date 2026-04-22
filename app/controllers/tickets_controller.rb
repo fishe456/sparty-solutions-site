efc0af636136f5b32485f5bb4d4448447b2c573b
Angie <popeian1@msu.edu>
2026-04-22 13:49:36 +0000

    Added css and other pages

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,7 @@
 class TicketsController < ApplicationController
   def index
   end
+  def show
+    @tickets = Ticket.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get "/tickets", to: "tikets#index"
+  get "/tickets", to: "tickets#index"
+
+  get "/tickets/new", to: "tickets#new"
   post "/tickets", to: "tickets#create"
+
+  get "/tickets/:id", to: "tickets#show"
+
+  get "/tickets/:id/edit", to: "tickets#edit"
+  patch "/tickets/:id", to: "tickets#update"
+  put "/tickets/:id", to: "tickets#update"
+
+  delete "/tickets/:id", to: "tickets#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
