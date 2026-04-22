a2513b35ead17b158adf40f6262bce2bb9dba854
Angie <popeian1@msu.edu>
2026-04-22 20:48:24 +0000

    Made a lot of progress, almost done

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,15 @@
 class TicketsController < ApplicationController
+  def home
+  end
+  def faq
+  end
   def index
+    @tickets = Ticket.all
   end
   def show
     @tickets = Ticket.find(params[:id])
+  end
+  def new
+    @tickets = Ticket.new
   end
 end
